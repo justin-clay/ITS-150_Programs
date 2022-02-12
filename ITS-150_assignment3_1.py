f4a647c99c168ff54b25cf176922f70c1498086e
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-02-11 23:37:43 -0500

    initial commit

--- ITS-150_assignment3_1.py
+++ ITS-150_assignment3_1.py
@@ -1,19 +1,28 @@
+## This program will prompt the user to enter a radius for a circle.
+## It will then calculate and print out the area and perimeter of the circle
 
-# A=πr^2
+
+# This creates a function to calculate the area using the formula A=πr^2
+# The circle radius is passed as a parameter from main() 
 def circleArea(radius):
     area = 3.141592653589793 * radius ** 2
     return area
 
-# P=2πr
+# This creates a function to calculate circle perimeter using this formula P=2πr
+#  The circle radius is passed as a parameter from main()
 def circlePerimeter(radius):
     perimeter = 2 * 3.141592653589793 * radius
     return perimeter
 
+# This creates the main() program funciton.
+# This will prompt the user for a circle radius, pass it to the two functions, 
+# and return and print the area and perimeter results.
 def main():
     circleRadius = eval(input("\nEnter the radius of the circle: "))
     print("Area of the circle is", round(circleArea(circleRadius), 1))
     print("Perimeter of the circle is", round(circlePerimeter(circleRadius), 1))
     print()
 
-main()
+# Runs the main function
+main() 
 
