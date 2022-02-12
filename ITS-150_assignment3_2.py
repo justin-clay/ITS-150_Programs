91a27433bdbaebaaf54f52f91925b6c9b527f6ca
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-02-12 01:12:59 -0500

    created functional code

--- ITS-150_assignment3_2.py
+++ ITS-150_assignment3_2.py
@@ -1,4 +1,22 @@
 
 
-def some_series():
-    
+def some_series(n):
+    x = 1
+    counter = 0
+    while counter <= n:
+        counter += 1
+        num = 1
+        for digit in str(x):
+            if int(digit) != 0:
+                num *= int(digit)
+        num += x  
+        x = num
+        if counter + 1 == n:
+            return x
+
+def main():
+    count = int(input("Enter a positive integer: "))
+    print("\nThe", str(count) + "th", "number of the series would be", some_series(count))
+    print()
+
+main()
