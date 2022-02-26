05f50df404becd3a3aaeeed19748367ea724756d
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-02-26 16:55:38 -0500

    added ITS-150 assign4

--- ITS-150_assignment3_2.py
+++ ITS-150_assignment3_2.py
@@ -1,5 +1,9 @@
+## This program creates a series of numbers and then prompts the user to enter a number.
+## Then the program will return the value of the series on the iteration that matches the user input.
 
-
+# Creates a function that will take number x and multiply each digit of x that is not a zero.
+# Then take that result and add it to the number x. That becomes the next number in the series.
+# Then the cycle repeats however many times the user prompts it to run.
 def some_series(n):
     x = 1
     counter = 0
@@ -13,10 +17,12 @@ def some_series(n):
         x = num
         if counter + 1 == n:
             return x
-
+# Create a main function that will prompt the user to enter a number.
+# That number is then passes to some_series() through the n parameter. 
+# Program will then print the number of the series iteration, and the value at that location of the series.
 def main():
     count = int(input("Enter a positive integer: "))
     print("\nThe", str(count) + "th", "number of the series would be", some_series(count))
     print()
-
+# Runs main program
 main()
