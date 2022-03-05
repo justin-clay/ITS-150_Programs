ae385db5b240493da834fd1bf24af18fb741e990
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-03-05 11:40:51 -0500

    finished logical build - still needs comments

--- ITS-150_assignment5_1.py
+++ ITS-150_assignment5_1.py
@@ -1,1 +1,14 @@
 
+def main():
+    while True:
+        try:
+            num = float(input("Enter a floating point number between 10.0 and 20.0 (both inclusive): "))
+            if num >= 10.0 and num <= 20.0:
+                print("Your number is", str(num) + ".")
+                break
+            else:
+                print("You did not enter a floating point number in the given range.")
+        except ValueError:
+            print("You did not enter a floating point number in the given range.")
+
+main()
