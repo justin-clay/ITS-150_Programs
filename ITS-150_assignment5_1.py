c0481a6a041f67e591d8e18a21e7d36d2b448c88
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-03-06 21:06:05 -0500

    finished comments

--- ITS-150_assignment5_1.py
+++ ITS-150_assignment5_1.py
@@ -1,14 +1,16 @@
+## This program will ask the user to input a floatin point number between 10.0 and 20.0
+## and then check to see if it matches that range.
 
 def main():
-    while True:
-        try:
+    while True: # loop will run until correct numbers are entered or key interupt is used
+        try: # will ask for float input and check if it is within range
             num = float(input("Enter a floating point number between 10.0 and 20.0 (both inclusive): "))
-            if num >= 10.0 and num <= 20.0:
+            if num >= 10.0 and num <= 20.0: # if number in range then the message is printed out and the loop broken
                 print("Your number is", str(num) + ".")
                 break
-            else:
+            else: # if num not in range then error message printed and loop starts again
                 print("You did not enter a floating point number in the given range.")
-        except ValueError:
+        except ValueError: # if a ValueError, such as from entering a string, then this message printed and restart loop
             print("You did not enter a floating point number in the given range.")
 
 main()
