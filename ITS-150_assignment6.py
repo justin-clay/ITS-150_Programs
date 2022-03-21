62c5d5df074e56122f95b732bd1d48b0de51d5e6
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-03-20 20:14:41 -0400

    added comments to assignment 6

--- ITS-150_assignment6.py
+++ ITS-150_assignment6.py
@@ -1,15 +1,20 @@
+# This program file has one parent class "Shape" and three 
+# child class Square, Rectangle, and Circle.
 
-
+# creates a Shape class that is passed dimensions in the for of parameters
+# and then passes those dimensions to the child classes when called upon. 
+# Then it will print the area and perimeter of shape that was called.
 class Shape:
-
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
-        self._radius = width
+        self._width = width # sets width parameter to the self variable
+        self._height = height # sets height parameter to the self variable
+        self._radius = width # uses the width parameter to pass to circle radius from user input. 
 
-    def __str__(self):
-        return "The area is " + str(self.area()) + " and perimeter is " + str(self.perimeter()) + "."
+    def __str__(self): # will print this text string with the associated self.area and self.perimeter, whichever shape class is called.
+        return "The area is " + str("{:.2f}".format(self.area())) + " and perimeter is " + str("{:.2f}".format(self.perimeter())) + "."
 
+# creates the Square class using the passed dimensions and will calculate
+# and return self.area() and self.perimeter() to be used in parent __str__ method
 class Square(Shape):
     
     def perimeter(self):
@@ -18,6 +23,8 @@ class Square(Shape):
     def area(self):
          return self._width * self._width
 
+# creates the Rectangle class using the passed dimensions and will calculate
+# and return self.area() and self.perimeter() to be used in parent __str__ method
 class Rectangle(Shape):
 
     def perimeter(self):
@@ -26,6 +33,8 @@ class Rectangle(Shape):
     def area(self):
         return self._width * self._height
 
+# creates the Square class using the passed dimensions and will calculate
+# and return self.area() and self.perimeter() to be used in parent __str__ method
 class Circle(Shape):
     
     def perimeter(self):
@@ -36,16 +45,17 @@ class Circle(Shape):
         PI = 3.14
         return (self._radius ** 2) * PI
 
-
+# creates a main() function that will test the above classes by prompting
+# the user to input required dimensions for various shapes and them pass them to the appripriate classes
 def main():
     width = float(input("Enter the length of a square: "))
-    print(Square(width))
+    print(Square(width)) # will pass width to Square class and calculate area and perimeter
 
     width = float(input("Enter the width of a rectangle: "))
     height = float(input("Enter the height of a rectangle: "))
-    print(Rectangle(width, height))
+    print(Rectangle(width, height)) # will pass width to Rectangle class and calculate area and perimeter
   
     radius = float(input("Enter the radius of a circle: "))
-    print(Circle(radius))
-    
+    print(Circle(radius)) # will pass width to Circle class and calculate area and perimeter
+
 main()
