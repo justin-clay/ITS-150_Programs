fdeabe766aef665f5efc28400c5b1da0f719f5b1
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-03-22 16:45:54 -0400

    finished  logic build

--- Final/ITS-150_assignmentFP1.py
+++ Final/ITS-150_assignmentFP1.py
@@ -1,12 +1,17 @@
 
 
-from curses import has_extended_color_support
 
 
 def main():
     caff = getInput()
     halfCaffHours = caffeineLossHalf(caff)
     print("\nLess than half of it will be left after " + str(halfCaffHours) + " hours.")
+    eightHours = caffeineLossEightHours(caff)
+    print("\nThe amount of caffeine left in the body after 8 hours is: " + str("{:.2f}".format(eightHours)) + " mg")
+    tenHours = caffeineLossTenHours(caff)
+    print("\nThe amount remaining after 10 hours: " + str("{:.2f}".format(tenHours)) + " mg")
+    print()
+    
 
 def getInput():
     caffeine = eval(input("\nEnter the amount of caffeine in mg: "))
@@ -21,6 +26,25 @@ def caffeineLossHalf(startingCaff):
         hours += 1
 
     return hours
+
+# calculate caffeine loss after eight hours of time at 20% per hour
+def caffeineLossEightHours(startingCaff):
+    hours = 8
+    caffeine = startingCaff
+    for i in range(hours):
+        caffeine = caffeine  * .8
+    return caffeine
+
+# Assuming that the person drinks a cup of coffee at 8 am and then drinks a cup 
+# of coffee at the end of each hour until 6 pm, then calculate how much caffeine 
+# will be left after 10 hours.
+def caffeineLossTenHours(startingCaff):
+    caffeine = startingCaff
+    hours = 10
+    for i in range(hours):
+        caffeine = caffeine * .8
+        caffeine += startingCaff
+    return caffeine
 
 
 main()
