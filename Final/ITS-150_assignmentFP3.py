d4440ea543db9dc358d5a52e4959fe3c4461f54c
justin-clay <64446483+justin-clay@users.noreply.github.com>
2022-03-25 14:02:45 -0400

    added degreesDict.dat file

--- Final/ITS-150_assignmentFP3.py
+++ Final/ITS-150_assignmentFP3.py
@@ -1,1 +1,14 @@
-    
+import pickle
+
+
+
+
+def getDictionary(fileName):
+    infile = open(fileName, 'rb')
+    degrees = pickle.load(infile)
+    infile.close()
+    return degrees
+
+
+
+d = getDictionary()
